805103ad04b0a6c5b6bf88af0a21a9905edded00
gulsahsahin <guulssahh@gmail.com>
2022-09-26 19:12:52 +0300

    sdf

--- js/todo.js
+++ js/todo.js
@@ -1,11 +1,12 @@
 let name = prompt("Adınız nedir?")
 let info = document.querySelector("#myName")
+localStorage.setItem("name", name)
 info.innerHTML = `${name}`
 
 const addInput = document.querySelector('#add-task')
 
-const addBtn =  document.querySelector('#add-btn')
-const delTaskBtn  =  document.querySelector('#del-btn')
+const addBtn = document.querySelector('#add-btn')
+const delTaskBtn = document.querySelector('#del-btn')
 
 const renameBtn = document.querySelector('.rename-task')
 const updateBtn = document.querySelector('.update-task')
@@ -17,9 +18,9 @@ const tasks = document.querySelector('.tasks')
 //     addInput.value = ''
 // })
 
-function addTodo(todo){
-    let todoTask  = ` 
-                <div class="task">
+function addTodo(todo) {
+    let todoTask = ` 
+                <div class = "task">
 					<input type="text" id="added-task" name='todo' disabled value="${todo}">
                     <div>
                         <input type="button" value="✔️" name='update' title='update task' class="update-task">
@@ -27,16 +28,16 @@ function addTodo(todo){
                         <input type="button" value="❌" name='delete' title='delete task' class="del-task">
                     </div>
                 </div>
-                ` 
+                `
     tasks.innerHTML += todoTask
 }
 
-addBtn.addEventListener('click', ()=> {
+addBtn.addEventListener('click', () => {
     let todo = addInput.value
-    
-    if (todo.length < 3){
+
+    if (todo.length < 3) {
         alert('En az 3 karakter giriniz.')
-    }else{
+    } else {
         todo = todo.trim()
         console.log(todo)
         addTodo(todo)
@@ -46,38 +47,38 @@ addBtn.addEventListener('click', ()=> {
 })
 
 addInput.addEventListener("keyup", (event) => {
-    if(event.which === 13) {
+    if (event.which === 13) {
         addTodo(todo)
     }
 })
 
 
-function updateTodo(){
+function updateTodo() {
 
-    let task  = document.querySelectorAll('.task')
+    let task = document.querySelectorAll('.task')
 
     task.forEach((t) => {
         // console.log(t.children)
-        t.addEventListener('click', e =>{
+        t.addEventListener('click', e => {
 
-            if(e.target.classList.contains('rename-task')){
-                
+            if (e.target.classList.contains('rename-task')) {
+
                 console.log('rename')
-                if (t.children[0].disabled){
+                if (t.children[0].disabled) {
                     t.children[0].disabled = false
                 }
-            }else if(e.target.classList.contains('del-task')) {
+            } else if (e.target.classList.contains('del-task')) {
                 t.remove()
             }
-            
-            else if (e.target.classList.contains('update-task')){
+
+            else if (e.target.classList.contains('update-task')) {
                 console.log(t.children[0].disabled)
-                if (t.children[0].disabled == false){
+                if (t.children[0].disabled == false) {
                     t.children[0].disabled = true
                 }
             }
         })
 
     })
-    
+
 }
